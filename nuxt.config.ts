62fb1a38dc199bc256e50ed185f15e9a5fd5bdf3
James <szabo.janos@thisisdone.com>
2024-10-11 08:45:35 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
             apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
         },
     },
-
     components: [
         {
             path: '~/components',
